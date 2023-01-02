d2f1a76618250418defe7e9a9824ccc2cc37fa4e
Eymen Varilci <evarilci@gmail.com>
2023-01-02 21:13:16 +0300

    Initial commit. Storyboard deleted, folders added.

---
+++ ChitChat/ChitChat/Screens/LoginScreen/Controller/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChitChat
+//
+//  Created by Eymen Varilci on 2.01.2023.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
